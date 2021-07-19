89f47280a50561f9e076f24b107358067dfc5995
Ibrahim Ersu <imeu2005@gmail.com>
2021-07-19 16:19:05 -0400

    Git From Intellij

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Git Hubs ");
+        System.out.println("Hello Git Hubs2 ");
+        System.out.println("Hello Git Hubs3 ");
+
     }
 }
