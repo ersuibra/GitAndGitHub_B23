c1133c67754499833643ba12332fb2a43b96d429
Ibrahim Ersu <imeu2005@gmail.com>
2021-07-26 15:33:20 -0400

    Feature A is here

---
+++ src/day02/AWork.java
@@ -0,0 +1,9 @@
+package day02;
+
+public class AWork {
+    public static void main(String[] args) {
+        System.out.println("Track that thing");
+        System.out.println("Track that 2thing");
+
+    }
+}
