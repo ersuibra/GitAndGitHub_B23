c9bbfe701a6584f94307c400464205114f8a3c21
Ibrahim Ersu <imeu2005@gmail.com>
2021-07-29 16:18:00 -0400

    day03_commit 2 set3

--- src/day03/ConflictedElf.java
+++ src/day03/ConflictedElf.java
@@ -2,6 +2,9 @@ package day03;
 
 public class ConflictedElf {
     public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Wello");
+        System.out.println("Zello");
 
     }
 }
