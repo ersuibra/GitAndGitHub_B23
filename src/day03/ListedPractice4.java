870423f833d45c79d6972f697647018844f50f30
Ibrahim Ersu <imeu2005@gmail.com>
2021-07-29 14:42:38 -0400

    day03_commit 2 List3

--- src/day03/ListedPractice4.java
+++ src/day03/ListedPractice4.java
@@ -3,6 +3,8 @@ package day03;
 public class ListedPractice4 {
     public static void main(String[] args) {
         System.out.println("Loki was here");
+        System.out.println("Loki was here");
+        System.out.println("Loki was here");
 
     }
 }
