7c6373f6d2d76e29c67d6575d2a8cca1af88d837
Ibrahim Ersu <imeu2005@gmail.com>
2021-07-30 10:42:50 -0400

    day04_commit 2

--- src/day04/Conflict.java
+++ src/day04/Conflict.java
@@ -3,5 +3,6 @@ package day04;
 public class Conflict {
     public static void main(String[] args) {
         System.out.println("Head to head");
+        System.out.println("Read to head");
     }
 }
