34e85a3529883b9a3c31f479222679601e392f66
Ibrahim Ersu <imeu2005@gmail.com>
2021-07-30 10:44:35 -0400

    day04_commit 5 Conflict

--- src/day04/Conflict.java
+++ src/day04/Conflict.java
@@ -3,5 +3,7 @@ package day04;
 public class Conflict {
     public static void main(String[] args) {
         System.out.println("Head to head");
+        System.out.println("Zead to Fead");
+        System.out.println("Zead to Fead");
     }
 }
