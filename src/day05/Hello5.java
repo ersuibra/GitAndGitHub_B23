5b1ee68c234fa24cf599ddb7bab15b800a0a7687
Ibrahim Ersu <imeu2005@gmail.com>
2021-08-05 14:12:38 -0400

    day05 commit4

--- src/day05/Hello5.java
+++ src/day05/Hello5.java
@@ -4,6 +4,8 @@ public class Hello5 {
     public static void main(String[] args) {
         System.out.println("Hello1");
         System.out.println("Hello2");
+        System.out.println("Hello3");
+
 
     }
 }
